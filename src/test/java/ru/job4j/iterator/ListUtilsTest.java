ddc93774830f153ea39a5ffab0ddc29bd26adbc7
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-27 20:51:15 +0300

    7. ListIterator [#524525]

---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.iterator;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ListUtilsTest {
+
+    @Test
+    public void whenAddBefore() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(Arrays.asList(1, 2, 3), is(input));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddBeforeWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 3, 2);
+    }
+
+    @Test
+    public void whenAddAfterLast() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
+        ListUtils.addAfter(input,2, 3);
+        assertThat(Arrays.asList(0, 1, 2, 3), is(input));
+    }
+
+    @Test
+    public void removeIfLessThan2() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+        ListUtils.removeIf(input, integer -> integer < 2);
+        assertThat(Arrays.asList(2, 3), is(input));
+    }
+
+    @Test
+    public void whenValueEquals3ThanReplace5() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+        ListUtils.replaceIf(input, integer -> integer == 3, 5);
+        assertThat(Arrays.asList(0, 1, 2, 5), is(input));
+    }
+}
