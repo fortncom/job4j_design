62b4a83a28355c6e2b3c78d89507f89d6529dce0
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-28 20:32:21 +0300

    1. Реализовать коллекцию Set на массиве [#524550]

---
+++ src/test/java/ru/job4j/collection/set/SimpleSetTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection.set;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleSetTest {
+
+    @Test
+    public void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(1));
+        assertTrue(set.contains(1));
+        assertFalse(set.add(1));
+    }
+
+    @Test
+    public void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(null));
+        assertTrue(set.contains(null));
+        assertFalse(set.add(null));
+    }
+
+    @Test
+    public void whenNotContain() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(2));
+        assertFalse(set.contains(1));
+    }
+
+}
