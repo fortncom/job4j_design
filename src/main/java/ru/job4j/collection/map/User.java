a8816d340bfdd8594efd26534a510610f1469f05
Tatyana <anatoliy.gromov.service@gmail.com>
2021-05-01 11:40:20 +0300

    3. Переопределить только hashCode [#524555]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -1,8 +1,25 @@
 package ru.job4j.collection.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
+
+    public static void main(String[] args) {
+        User user = new User("Tom", 1,
+                new GregorianCalendar(1972, 12, 21));
+
+        User user2 = new User("Tom", 1,
+                new GregorianCalendar(1972, 12, 21));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user, new Object());
+        map.put(user2, new Object());
+        for (Map.Entry<User, Object> userObjectEntry : map.entrySet()) {
+            System.out.println(userObjectEntry);
+        }
+    }
 
     private String name;
     private int children;
@@ -26,4 +43,12 @@ public class User {
         return birthday;
     }
 
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
 }
