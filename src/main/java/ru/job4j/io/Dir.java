1312e3763aaced52d79cc82bd9748002bba9fdcc
Tatyana <anatoliy.gromov.service@gmail.com>
2021-05-09 22:55:11 +0300

    changed dir

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Dir {
 
     public static void main(String[] args) {
-        File file = new File(".");
+        File file = new File("c:\\projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
