704dad0b294cbc350d3e150ee078a7f9480334f8
Tatyana <anatoliy.gromov.service@gmail.com>
2021-05-09 13:16:34 +0300

    1. Читаем файл конфигурации [#524579]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "src/main/java/ru/job4j/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")
+                ,is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "src/main/java/ru/job4j/app.comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertNull(config.value("#hibernate.connection.password"));
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void whenPairWithoutValue() throws IllegalStateException {
+        String path = "src/main/java/ru/job4j/app.fail.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
