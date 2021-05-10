64eb0a7073288a1d4b9bbd903e6d9603253005e6
Tatyana <anatoliy.gromov.service@gmail.com>
2021-05-10 20:59:15 +0300

    Добавил StringBuilder

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,18 +5,22 @@ import java.io.FileOutputStream;
 public class ResultFile {
 
     public static void main(String[] args) {
+        StringBuilder rsl;
         try (FileOutputStream out = new FileOutputStream("src/main/resources/result.txt")) {
-            out.write("Hello, world!".getBytes());
+            rsl = new StringBuilder("Hello, world!");
+            out.write(rsl.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
         try (FileOutputStream out = new FileOutputStream("src/main/resources/multiplicationTable.txt")) {
+            rsl = new StringBuilder();
             for (int i = 1; i < 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    out.write((i * j + " ").getBytes());
+                    rsl.append(i * j).append(" ");
                 }
-                out.write(("\n").getBytes());
+                rsl.append(System.lineSeparator());
             }
+            out.write(rsl.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
