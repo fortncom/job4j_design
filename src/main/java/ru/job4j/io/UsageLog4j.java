7a3a6dc86c6da98f2ff0c9a02a45596771dc064b
Tatyana <anatoliy.gromov.service@gmail.com>
2021-05-14 20:23:39 +0300

    3. Slf4j - вывод переменных. [#524567]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,25 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte exception = 3;
+        short countException = 15;
+        LOG.error("Программа завершилась с ошибкой №{}. Количество критических завершений {}.",
+                exception, countException);
+        boolean debugOn = true;
+        long timeDebug = 32143244356243423L;
+        LOG.debug("Процесс отладки запущен? - {}. продолжительность {} мс.",
+                debugOn, timeDebug);
+        String name = "Tom";
+        char level = 'A';
+        int id = 242452352;
+        LOG.info("Пользователь {} с уровнем доступа {} и id {} зарегистрирован.",
+                name, level, id);
+        int success = 1284;
+        float averageSuccess = (success + countException) / countException;
+        LOG.info("Среднее количество успешных сборок на одно критическое завершение {}.",
+                averageSuccess);
+        double timeSession = 55.5;
+        LOG.warn("Превышено время работы сессии {}. Сессия завершена.",
+                timeSession);
     }
 }
