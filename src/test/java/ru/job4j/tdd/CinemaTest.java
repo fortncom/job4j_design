12270cdab3004f8e5765a20e9e99fadb23602594
Tatyana <anatoliy.gromov.service@gmail.com>
2021-06-29 22:38:44 +0300

    2. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenRowInvalid() {
+        Cinema cinema = new Cinema3D();
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1122, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenColumnInvalid() {
+        Cinema cinema = new Cinema3D();
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 11, -1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDateInvalid() {
+        Cinema cinema = new Cinema3D();
+        Account account = new AccountCinema();
+        Calendar date = Calendar.getInstance();
+        date.set(1990, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+}
