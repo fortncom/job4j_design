00c0e74f7ee0837954b892a3e0f5eb589d25f7c5
Tatyana <anatoliy.gromov.service@gmail.com>
2021-07-21 21:52:13 +0300

    split main

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -12,44 +12,38 @@ import java.util.Scanner;
 
 public class CSVReader {
 
-    public static void main(String[] args) {
-        if (args.length < 4) {
-            throw new IllegalArgumentException("Insufficient number of input parameters Usage java -jar dir.jar PATH DELIMITER OUT FILTER.");
-        }
-        ArgsName argsName = ArgsName.of(args);
-        Path start = Paths.get(argsName.get("path"));
-        if (!start.toFile().exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", start.toFile().getAbsoluteFile()));
-        }
-        if (start.toFile().isDirectory()) {
-            throw new IllegalArgumentException(String.format("File is directory %s", start.toFile().getAbsoluteFile()));
-        }
-        StringBuilder rsl = new StringBuilder();
-        List<String> filter = new ArrayList<>(List.of(argsName.get("filter").split(",")));
+    private StringBuilder rsl = new StringBuilder();
+    private ArgsName argsName;
+
+    public CSVReader(ArgsName argsName) {
+        this.argsName = argsName;
+    }
+
+    public ArgsName getArgsName() {
+        return argsName;
+    }
+
+    public List<Integer> indicesOf(String[] data) {
+        List<String> filter = new ArrayList<>(
+                List.of(argsName.get("filter").split(",")));
         List<Integer> indexes = new ArrayList<>();
-        String delimiter = argsName.get("delimiter");
-        try (Scanner scanner = new Scanner(start)) {
-            if (scanner.hasNextLine()) {
-                String[] line = scanner.nextLine().split(delimiter);
-                for (int i = 0; i < line.length; i++) {
-                    if (filter.contains(line[i])) {
-                        indexes.add(i);
-                        rsl.append(line[i]);
-                        rsl.append(delimiter);
-                    }
-                }
+        for (int i = 0; i < data.length; i++) {
+            if (filter.contains(data[i])) {
+                indexes.add(i);
             }
-            while (scanner.hasNextLine()) {
-                rsl.append(System.lineSeparator());
-                String[] line = scanner.nextLine().split(delimiter);
-                for (Integer index : indexes) {
-                    rsl.append(line[index]);
-                    rsl.append(delimiter);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        return indexes;
+    }
+
+    public void parseLine(String[] line, List<Integer> indexes) {
+        for (Integer index : indexes) {
+            rsl.append(line[index]);
+            rsl.append(argsName.get("delimiter"));
+        }
+        rsl.append(System.lineSeparator());
+    }
+
+    public void write() {
         String out = argsName.get("out");
         if (out.equalsIgnoreCase("stdout")) {
             System.out.println(rsl);
@@ -60,5 +54,36 @@ public class CSVReader {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            throw new IllegalArgumentException("Insufficient number of input parameters Usage java -jar dir.jar PATH DELIMITER OUT FILTER.");
+        }
+        CSVReader csv = new CSVReader(ArgsName.of(args));
+        Path start = Paths.get(csv.getArgsName().get("path"));
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toFile().getAbsoluteFile()));
+        }
+        if (start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("File is directory %s", start.toFile().getAbsoluteFile()));
+        }
+        List<Integer> indexes;
+        String delimiter = csv.getArgsName().get("delimiter");
+        try (Scanner scanner = new Scanner(start)) {
+            if (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split(delimiter);
+                indexes = csv.indicesOf(line);
+                csv.parseLine(line, indexes);
+            }
+            while (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split(delimiter);
+                indexes = csv.indicesOf(line);
+                csv.parseLine(line, indexes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        csv.write();
     }
 }
